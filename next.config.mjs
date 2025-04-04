4d40158d34ea8b16a43325deedd814f3a34dc952
moazzamali5 <moazzamaligujjar5@gmail.com>
2025-04-05 02:04:55 +0800

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',  // Enable static exports
+  basePath: '/Escort', // Your repository name
   images: {
     unoptimized: true,
-    domains: ['localhost'],
   },
+  assetPrefix: '/Escort/', // Your repository name with trailing slash
 };
 
 export default nextConfig; 
